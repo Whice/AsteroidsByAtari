b55b53c6a229886ab3fbecfc7a808084abf16567
A.Baranov <a.baranov@overmindgroup.com>
2022-04-18 10:38:32 +0300

    Скорректирован тип космических объектов.

--- Assets/SpaceModel/SpaceObjectType.cs
+++ Assets/SpaceModel/SpaceObjectType.cs
@@ -9,29 +9,45 @@
         /// Неопознано.
         /// </summary>
         unknow = 0,
+
+        #region Опасные объекты.
+
         /// <summary>
         /// Большой астероид.
         /// </summary>
-        bigAsteroid = 1,
+        bigAsteroid = unknow + 1,
         /// <summary>
         /// Маленький астероид - часть большого.
         /// </summary>
-        asteroidShard = 2,
-        /// <summary>
-        /// Игрок.
-        /// </summary>
-        player = 3,
+        asteroidShard = bigAsteroid + 1,
         /// <summary>
         /// Корабль пришельцев. Назвал стереотипно НЛО.
         /// </summary>
-        nlo = 4,
+        nlo = asteroidShard + 1,
+
+        #endregion Опасные объекты.
+
+        #region Типы игрока.
+
+        /// <summary>
+        /// Игрок.
+        /// </summary>
+        player = nlo + 1,
         /// <summary>
         /// Обычная пуля игрока.
         /// </summary>
-        simpleBullet = 5,
+        simpleBullet = player + 1,
         /// <summary>
         /// Лазерный луч игрока.
         /// </summary>
-        laser = 6
+        laser = simpleBullet + 1,
+
+        #endregion Типы игрока.
+
+        /// <summary>
+        /// Это не тип, а номер, следующий за номером последнего типа.
+        /// <br/>Например, если последний тип имеет номер 8, то <see cref="end"/> будет равен 9.
+        /// </summary>
+        end = laser + 1
     }
 }
